6786b7c35c31077b28aa3075b5f0075a476598b5
jakub2929 <jakub2929@seznam.cz>
2025-10-14 20:25:02 +0200

    Update HiddenNetlifyInquiryForm component

--- src/app/HiddenNetlifyInquiryForm.tsx
+++ src/app/HiddenNetlifyInquiryForm.tsx
@@ -1,7 +1,7 @@
 // This hidden form ensures Netlify detects the inquiry form for submissions from JS-rendered modals.
 export default function HiddenNetlifyInquiryForm() {
   return (
-    <form name="inquiry" netlify="true" hidden>
+  <form name="inquiry" data-netlify="true" hidden>
       <input type="text" name="name" />
       <input type="text" name="contact" />
       <textarea name="message" />
