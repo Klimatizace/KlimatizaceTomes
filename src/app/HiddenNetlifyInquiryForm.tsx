18bf0afc3bd74445b1cc6afff28af466e29e9512
jakub2929 <jakub2929@seznam.cz>
2025-10-14 20:46:46 +0200

    fix: update HiddenNetlifyInquiryForm component

--- src/app/HiddenNetlifyInquiryForm.tsx
+++ src/app/HiddenNetlifyInquiryForm.tsx
@@ -3,7 +3,16 @@ import * as React from 'react';
 
 export default function HiddenNetlifyInquiryForm() {
   return (
-    <form name="inquiry" data-netlify="true" hidden>
+    <form name="inquiry" method="POST" data-netlify="true" netlify-honeypot="bot-field" hidden>
+      <input type="hidden" name="form-name" value="inquiry" />
+      <input type="hidden" name="subject" value="Nová poptávka z klimatizacetomes.netlify.app" />
+      <p className="hidden">
+        <label>
+          Don't fill this out if you're human:
+          {' '}
+          <input name="bot-field" />
+        </label>
+      </p>
       <input type="text" name="name" />
       <input type="text" name="contact" />
       <textarea name="message" />
